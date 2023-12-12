54a60d68696fd55a4d5b4d30a29bb9925aac4076
Horn-Leo1015 <153061347+Horn-Leo1015@users.noreply.github.com>
2023-12-12 07:02:10 +0000

    あ

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,31 +1,19 @@
-# Streamlitライブラリをインポート
 import streamlit as st
+import random
 
-# ページ設定（タブに表示されるタイトル、表示幅）
-st.set_page_config(page_title="タイトル", layout="wide")
+# 乱数を生成して運勢を返す関数
+def get_fortune(random_number):
+    fortunes = ["大吉", "中吉", "小吉", "吉", "凶"]
+    return fortunes[random_number % 5]
 
-# タイトルを設定
-st.title('Streamlitのサンプルアプリ')
+# Webアプリケーションのタイトルを設定
+st.title('今日の運勢を占う')
 
-# テキスト入力ボックスを作成し、ユーザーからの入力を受け取る
-user_input = st.text_input('あなたの名前を入力してください')
+# 乱数の入力フォームを作成
+random_number = st.number_input('1から100の間で好きな数字を入力してください', min_value=1, max_value=100)
 
-# ボタンを作成し、クリックされたらメッセージを表示
-if st.button('挨拶する'):
-    if user_input:  # 名前が入力されているかチェック
-        st.success(f'🌟 こんにちは、{user_input}さん! 🌟')  # メッセージをハイライト
-    else:
-        st.error('名前を入力してください。')  # エラーメッセージを表示
-
-# スライダーを作成し、値を選択
-number = st.slider('好きな数字（10進数）を選んでください', 0, 100)
-
-# 補足メッセージ
-st.caption("十字キー（左右）でも調整できます。")
-
-# 選択した数字を表示
-st.write(f'あなたが選んだ数字は「{number}」です。')
-
-# 選択した数値を2進数に変換
-binary_representation = bin(number)[2:]  # 'bin'関数で2進数に変換し、先頭の'0b'を取り除く
-st.info(f'🔢 10進数の「{number}」を2進数で表現すると「{binary_representation}」になります。 🔢')  # 2進数の表示をハイライト
+# 占いボタンが押されたときの処理
+if st.button('占う'):
+    # 乱数に応じた運勢を取得
+    fortune = get_fortune(random_number)
+    st.write(f'あなたの今日の運勢は...{fortune}です！')
